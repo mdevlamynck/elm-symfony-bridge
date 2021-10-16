70c9c4bcb6fdf894ad95c435df0f72db90892e39
Matthias Devlamynck <matthias.devlamynck@mailoo.org>
2021-10-16 16:38:01 +0200

    Import parser tests from format-message tests

---
+++ tests/Translation/IntlIcu/TranspilerTest.elm
@@ -0,0 +1,9 @@
+module Translation.IntlIcu.TranspilerTest exposing (suite)
+
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Converts a translation json to an elm module" <|
+        []
